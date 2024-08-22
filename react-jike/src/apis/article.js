abedd07ce44a19cc5dccd348a7b88bc8f52da86f
liuwei <1454294995@qq.com>
2024-08-22 14:08:26 +0800

    2024.08.22_01

--- react-jike/src/apis/article.js
+++ react-jike/src/apis/article.js
@@ -1,5 +1,8 @@
 import { request } from '@/utils'
 
 export const getChannelApi = () => request({ url: '/channels', method: 'get' })
-export const addArticleApi = (data) => request({ url: '/mp/articles', method: 'post', data })
-export const getArticleListApi = (params) => request({ url: '/mp/articles', method: 'get', params })
+export const addArticleApi = data => request({ url: '/mp/articles', method: 'post', data })
+export const getArticleListApi = params => request({ url: '/mp/articles', method: 'get', params })
+export const deleteArticleApi = id => request({ url: `/mp/articles/${id}`, method: 'delete' })
+export const getArticleDetailApi = id => request({ url: `/mp/articles/${id}`, method: 'get' })
+export const updateArticleApi = data => request({ url: `/mp/articles/${data.id}?draft=false`, method: 'put', data })
--- react-jike/src/pages/Publish/index.jsx
+++ react-jike/src/pages/Publish/index.jsx
@@ -1,11 +1,11 @@
 import { Card, Breadcrumb, Form, Button, Radio, Input, Upload, Space, Select, message } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import React from 'react'
-import { addArticleApi } from '@/apis/article'
+import { addArticleApi, getArticleDetailApi, updateArticleApi } from '@/apis/article'
 import { useChannel } from '@/hooks/useChannel'
 
 const { Option } = Select
@@ -14,16 +14,16 @@ export default function Publish() {
   const { channelList } = useChannel()
 
   const [imageType, setImageType] = React.useState(0)
-  const onTypeChange = (e) => {
+  const onTypeChange = e => {
     setImageType(e.target.value)
   }
 
   const [imageList, setImageList] = React.useState([])
-  const onUploadChange = (values) => {
+  const onUploadChange = values => {
     setImageList(values.fileList)
   }
 
-  const onFinish = async (values) => {
+  const onFinish = async values => {
     if (imageType !== imageList.length) return message.warning('图片类型和图片数量不匹配')
     const { title, channel_id, content } = values
     const reqData = {
@@ -32,31 +32,69 @@ export default function Publish() {
       channel_id,
       cover: {
         type: imageType,
-        images: imageList.map((item) => item.response.data.url),
-      },
+        images: imageList.map(item => {
+          if (item.response) {
+            return item.response.data.url
+          } else {
+            return item.url
+          }
+        })
+      }
     }
-    const res = await addArticleApi(reqData)
-    console.log('res', res)
+    if (articleId) {
+      updateArticleApi({ ...reqData, id: articleId })
+    } else {
+      const res = await addArticleApi(reqData)
+      console.log('res', res)
+    }
   }
 
+  // 回填数据
+  const [searchParams] = useSearchParams() // 获取路由参数
+  const articleId = searchParams.get('id')
+  const [form] = Form.useForm()
+  React.useEffect(() => {
+    const getArticleDetail = async () => {
+      const res = await getArticleDetailApi(articleId)
+      form.setFieldsValue({ ...res.data, type: res.data.cover.type })
+      setImageType(res.data.cover.type)
+      setImageList(res.data.cover.images.map(item => ({ url: item })))
+    }
+    if (articleId) {
+      getArticleDetail()
+    }
+  }, [articleId, form])
+
   return (
-    <div className='publish'>
-      <Card title={<Breadcrumb items={[{ title: <Link to={'/'}>首页</Link> }, { title: '发布文章' }]} />}>
-        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={{ type: 0 }} onFinish={onFinish}>
-          <Form.Item label='标题' name='title' rules={[{ required: true, message: '请输入文章标题' }]}>
-            <Input placeholder='请输入文章标题' style={{ width: 400 }} />
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb
+            items={[{ title: <Link to={'/'}>首页</Link> }, { title: `${articleId ? '修改' : '发布'}文章` }]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 0 }}
+          onFinish={onFinish}
+          form={form}
+        >
+          <Form.Item label="标题" name="title" rules={[{ required: true, message: '请输入文章标题' }]}>
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
           </Form.Item>
-          <Form.Item label='频道' name='channel_id' rules={[{ required: true, message: '请选择文章频道' }]}>
-            <Select placeholder='请选择文章频道' style={{ width: 400 }}>
-              {channelList.map((item) => (
+          <Form.Item label="频道" name="channel_id" rules={[{ required: true, message: '请选择文章频道' }]}>
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              {channelList.map(item => (
                 <Option key={item.id} value={item.id}>
                   {item.name}
                 </Option>
               ))}
             </Select>
           </Form.Item>
-          <Form.Item label='封面'>
-            <Form.Item name='type'>
+          <Form.Item label="封面">
+            <Form.Item name="type">
               <Radio.Group onChange={onTypeChange}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
@@ -65,13 +103,14 @@ export default function Publish() {
             </Form.Item>
             {imageType > 0 && (
               <Upload
-                listType='picture-card'
+                listType="picture-card"
                 showUploadList
                 action={'http://geek.itheima.net/v1_0/upload'}
-                name='image'
+                name="image"
                 onChange={onUploadChange}
                 maxCount={imageType}
                 multiple={imageType > 1}
+                fileList={imageList}
               >
                 <div style={{ marginTop: 8 }}>
                   <PlusOutlined />
@@ -79,13 +118,13 @@ export default function Publish() {
               </Upload>
             )}
           </Form.Item>
-          <Form.Item label='内容' name='content' rules={[{ required: true, message: '请输入文章内容' }]}>
-            <ReactQuill className='publish-quill' theme='snow' placeholder='请输入文章内容' />
+          <Form.Item label="内容" name="content" rules={[{ required: true, message: '请输入文章内容' }]}>
+            <ReactQuill className="publish-quill" theme="snow" placeholder="请输入文章内容" />
           </Form.Item>
 
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
-              <Button size='large' type='primary' htmlType='submit'>
+              <Button size="large" type="primary" htmlType="submit">
                 发布文章
               </Button>
             </Space>
